4a594b47380d0c8eeb419b07c0fc9dad20b6da77
Khanh <NKQKhanhs@HONEY>
2024-12-11 19:03:05 +0700

    update thêm,sửa,xóa,chi tiết menu

--- Harmic/obj/Debug/net9.0/Harmic.AssemblyInfo.cs
+++ Harmic/obj/Debug/net9.0/Harmic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7f8ec4845ad38c05fb04c14c7c7c17e241adf06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709f4daa1d0fcce5726b55bd41ce211ed02612bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
