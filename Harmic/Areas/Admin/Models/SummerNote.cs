e14b037f438c97c29b9916bc1156fdee242dc777
Khanh <NKQKhanhs@HONEY>
2024-12-13 22:34:07 +0700

    th11

---
+++ Harmic/Areas/Admin/Models/SummerNote.cs
@@ -0,0 +1,25 @@
+﻿namespace Harmic.Areas.Admin.Models
+{
+    public class SummerNote
+    {
+        public SummerNote(string idEditor, bool loadLibrary = true)
+        {
+            IDEditor = idEditor;
+            LoadLibary = loadLibrary;
+        }
+        public string IDEditor { get; set; }
+        public bool LoadLibary { get; set; }
+        public int Height { get; set; } = 500;
+        public string toolBar { get; set; } = @"
+            [
+                ['style' , ['style']],
+                ['font' , ['bold','underline','clear']],
+                ['color' , ['color']],
+                ['para' , ['ul','uo','paragraph']],
+                ['table' , ['table']],
+                ['insert' , ['link','elfindFiles','video','elfinder']],
+                ['view' , ['fullscreen','codeview','help']],
+            ]
+        ";
+    }
+}
--- Harmic/obj/Debug/net9.0/Harmic.AssemblyInfo.cs
+++ Harmic/obj/Debug/net9.0/Harmic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a594b47380d0c8eeb419b07c0fc9dad20b6da77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d76a0f89b9435c875219c2ed7dadd241bf91484")]
 [assembly: System.Reflection.AssemblyProductAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
